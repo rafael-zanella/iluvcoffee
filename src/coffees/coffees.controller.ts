67605c52eaffad62a857c2dff82aeb16a7e5ada7
Rafael Scotti <rafaels.zanella@gmail.com>
2020-08-30 02:47:06 -0300

    merge(develop): add first and second sections.

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Controller, Get, Param, Body, Post, Patch, Delete, Query } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    //const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id : string) : Coffee {
+    return this.coffeesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    return this.coffeesService.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
+}
