f50df1b2f593813e5eb83186a8d751b38aa7e656
Rafael Zanella <rafaels.zanella@gmail.com>
2020-12-15 21:06:09 -0300

    feat(Entity Index): add indexes to Event entity.

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 
+@Index(['name', 'type'])
 @Entity()
 export class Event {
 
